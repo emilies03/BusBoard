5d02fba06732160b1be58064e8dac2ebb3f871fb
emilies03 <Shelly7xxx>
2020-07-09 11:24:26 +0100

    TFL server querying and returning correct JSON object

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +14,22 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
+      ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+      
+      
+      var clientResponse = getClientJSONResponse();
     }
+
+    private static IRestResponse getClientJSONResponse()
+    {
+      var client = new RestClient("https://api.tfl.gov.uk");
+      var request = new RestRequest("StopPoint/490008660N/Arrivals", DataFormat.Json);
+      var response = client.Get(request);
+      
+      Console.WriteLine(response.ResponseUri);
+
+      return response;
+    }
+
   }
 }
