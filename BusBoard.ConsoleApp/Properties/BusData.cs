59c4964f6f5a74d95ddc372ab3ce122e0430e87f
emilies03 <Shelly7xxx>
2020-07-09 11:39:54 +0100

    Bus data class created

---
+++ BusBoard.ConsoleApp/Properties/BusData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BusBoard.ConsoleApp.Properties
+{
+    public class BusData
+    {
+        private string VehicleID { get; set; }
+        private string LineID { get; set; }
+        private string DestinationName { get; set; }
+        private int TimeToStation { get; set; }
+        private DateTime ExpectedArrivalTime { get; set; }
+
+        [JsonConstructor]
+        public BusData(string vehicleId, string lineId, string destinationName, string timeToStation,
+            string expectedArrivalTime)
+        {
+            VehicleID = vehicleId;
+            LineID = lineId;
+            DestinationName = destinationName;
+            TimeToStation = int.Parse(timeToStation);
+            ExpectedArrivalTime = Convert.ToDateTime(expectedArrivalTime);
+        }
+    }
+}
