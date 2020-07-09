579710e9b941976affcc6c6ee290891bc40f6428
emilies03 <estone97@gmail.com>
2020-07-09 12:01:18 +0100

    List of arriving buses printed to comsole

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using BusBoard.ConsoleApp.Properties;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using RestSharp.Authenticators;
@@ -15,7 +17,27 @@ namespace BusBoard.ConsoleApp
     static void Main(string[] args)
     {
       ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-      var clientResponse = getClientJSONResponse();
+
+      var clientJSONResponse = getClientJSONResponse();
+      List<BusData> listArrivingBuses = getArrivingBussesFromJSON(clientJSONResponse);
+      printBusesToConsole(listArrivingBuses);
+    }
+
+    private static void printBusesToConsole(List<BusData> listArrivingBuses)
+    {
+      Console.WriteLine($"The next 5 buses arriving at {listArrivingBuses[0].StationName} are: \n");
+      foreach (var bus in listArrivingBuses)
+      {
+        Console.WriteLine(bus.VehicleID);
+      }
+    }
+
+
+    private static List<BusData> getArrivingBussesFromJSON(IRestResponse clientJSONResponse)
+    {
+      List<BusData> listArrivingBusDataFromJSON = JsonConvert.DeserializeObject<List<BusData>>(clientJSONResponse.Content);
+
+      return listArrivingBusDataFromJSON;
     }
 
     private static IRestResponse getClientJSONResponse()
@@ -23,8 +45,6 @@ namespace BusBoard.ConsoleApp
       var client = new RestClient("https://api.tfl.gov.uk");
       var request = new RestRequest("StopPoint/490008660N/Arrivals", DataFormat.Json);
       var response = client.Get(request);
-      
-      Console.WriteLine(response.ResponseUri);
 
       return response;
     }
