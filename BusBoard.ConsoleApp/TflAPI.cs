c8b2b02d698a18b250cb84fccc9f2359d6343d77
emilies03 <estone97@gmail.com>
2020-07-09 14:57:04 +0100

    tfl API logic moved to separate class - 2nd attempt

--- BusBoard.ConsoleApp/TflAPI.cs
+++ BusBoard.ConsoleApp/TflAPI.cs
@@ -1,7 +1,22 @@
-﻿namespace BusBoard.ConsoleApp
+﻿using System.Collections.Generic;
+using System.Linq;
+using BusBoard.ConsoleApp.Properties;
+using RestSharp;
+
+namespace BusBoard.ConsoleApp
 {
     public class TflAPI
     {
-        
+        public IEnumerable<BusData> GetArrivingBusesListFromServer(string userInput)
+        {
+            var client = new RestClient("https://api.tfl.gov.uk");
+            var request = new RestRequest($"StopPoint/{userInput}/Arrivals");
+            List<BusData> listArrivingBusDataFromJSON = client.Execute<List<BusData>>(request).Data;
+            var orderedList5ArrivingBusData =
+                (listArrivingBusDataFromJSON.OrderBy(o => o.ExpectedArrival).ToList()).Take(5);
+
+            return orderedList5ArrivingBusData;
+        }
+
     }
 }
