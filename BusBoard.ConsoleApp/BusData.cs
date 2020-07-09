fd03aaa021af21d21a9033bbf5837fb3de9f6185
emilies03 <Shelly7xxx>
2020-07-09 14:46:39 +0100

    code review fixes, deserialization straight from client, code clean up and only 5 buses printed out

---
+++ BusBoard.ConsoleApp/BusData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BusBoard.ConsoleApp.Properties
+{
+    public class BusData
+    {
+        public string VehicleID { get; set; }
+        public string LineID { get; set; }
+        public string StationName { get; set; }
+        public string DestinationName { get; set; }
+        public int TimeToStation { get; set; }
+        public DateTime ExpectedArrival { get; set; }
+    }
+}
