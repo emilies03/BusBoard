0afb5836ad7feacf78fd5393dd2f829ece597085
emilies03 <estone97@gmail.com>
2020-07-10 14:50:06 +0100

    Automatic refresh every 30 secs

--- BusBoard.Web/Controllers/HomeController.cs
+++ BusBoard.Web/Controllers/HomeController.cs
@@ -16,12 +16,10 @@ namespace BusBoard.Web.Controllers
         [HttpGet]
         public ActionResult BusInfo(PostcodeSelection selection)
         {
-            // Add some properties to the BusInfo view model with the data you want to render on the page.
-            // Write code here to populate the view model with info from the APIs.
-            // Then modify the view (in Views/Home/BusInfo.cshtml) to render upcoming buses.
             try
             {
                 var info = new BusInfo(selection.Postcode);
+                Response.AddHeader("Refresh", "30");
                 return View(info);
             }
             catch (InvalidPostcodeException invalidPostcodeException)
